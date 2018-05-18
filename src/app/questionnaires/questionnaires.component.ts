4b4332546b9278cc6a33ae70c175f5cb9dcdb8bc
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-05-18 17:03:18 +0800

    Add function to automatically refresh list after add/edit

--- src/app/questionnaires/questionnaires.component.ts
+++ src/app/questionnaires/questionnaires.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { QuestionnairesService } from '../_services/questionnaires.service';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { QuestionnaireAddEditComponent } from './questionnaire-add-edit/questionnaire-add-edit.component';
 import { QuestionnaireShowComponent } from './questionnaire-show/questionnaire-show.component';
+import { Observable, Subscription } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-questionnaires',
@@ -16,10 +18,12 @@ export class QuestionnairesComponent implements OnInit {
   errorMsg;
   questionnaires: any;
   questionnaire: any = {};
-	bsModalRef: BsModalRef;
+  bsModalRef: BsModalRef;
+  subscriptions: Subscription[] =[];
 
-  constructor( private _questionnaireService: QuestionnairesService,
-               private modalService: BsModalService ) { }
+  constructor(private _questionnaireService: QuestionnairesService,
+              private modalService: BsModalService,
+              private changeDetection: ChangeDetectorRef ) { }
 
   ngOnInit() {
     this.getAllQuestionnaires();
@@ -32,12 +36,13 @@ export class QuestionnairesComponent implements OnInit {
         error => this.errorMsg = error);
   }
 
-	// modal display
+	// MODAL DISPLAY
 
 	addEditQuestionnaire(questionnaire) {
     const initialState = {
       questionnaire: (questionnaire) ? questionnaire : {}
-    }    
+    }
+    this.subscribeModal();
     this.questionnaireAddEditModal = this.modalService.show(
       QuestionnaireAddEditComponent, 
       Object.assign({initialState}, { class: 'modal-lg' })
@@ -52,6 +57,37 @@ export class QuestionnairesComponent implements OnInit {
       QuestionnaireShowComponent, 
       Object.assign({initialState}, { class: 'modal-lg' })
     );
+  }
+
+  subscribeModal() {
+    const _combine = combineLatest(Observable,
+      this.modalService.onHide, 
+      this.modalService.onHidden
+    ).subscribe(() => this.changeDetection.markForCheck());
+
+    this.subscriptions.push(
+      this.modalService.onHide.subscribe((reason: string) => {
+        this._questionnaireService.getAllQuestionnaires()
+          .subscribe(data =>
+            this.questionnaires = data, 
+            error => this.errorMsg = error);
+      })
+    );
+
+    this.subscriptions.push(
+      this.modalService.onHidden.subscribe((reason: string) => {
+        this.unsubscribeModal();
+      })
+    )
+
+    this.subscriptions.push(_combine);
+  }
+
+  unsubscribeModal() {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+    this.subscriptions = [];
   }
 
 }
